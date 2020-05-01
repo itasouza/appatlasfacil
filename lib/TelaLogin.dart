ca42a093e5a55b57c0a9bab3e95fb7b5d56e4437
itasouza <itasouza@yahoo.com.br>
2020-05-01 00:02:44 -0300

    Tela de login pronta
    Este e um teste criando uma página de login nova e usando o modelo

--- lib/TelaLogin.dart
+++ lib/TelaLogin.dart
@@ -1,6 +1,7 @@
+import 'package:appatlasfacil/config/ui_icons.dart';
+import 'package:appatlasfacil/src/widgets/SocialMediaWidget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:appatlasfacil/config/app_config.dart' as config;
-import 'package:appatlasfacil/route_generator.dart';
 
 
 class TelaLogin extends StatefulWidget {
@@ -9,58 +10,134 @@ class TelaLogin extends StatefulWidget {
 }
 
 class _TelaLoginState extends State<TelaLogin> {
-  // This widget is the root of your application.
-  @override
+  bool _showPassword = false;
+
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Business Listing,Directory Flutter UI KIT',
-      initialRoute: '/',
-      onGenerateRoute: RouteGenerator.generateRoute,
-      debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData(
-        fontFamily: 'Poppins',
-        primaryColor: Color(0xFF252525),
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: Color(0xFF2C2C2C),
-        accentColor: config.Colors().mainDarkColor(1),
-        hintColor: config.Colors().secondDarkColor(1),
-        focusColor: config.Colors().accentDarkColor(1),
-        textTheme: TextTheme(
-          button: TextStyle(color: Color(0xFF252525)),
-          headline: TextStyle(fontSize: 20.0, color: config.Colors().secondDarkColor(1)),
-          display1: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600, color: config.Colors().secondDarkColor(1)),
-          display2: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600, color: config.Colors().secondDarkColor(1)),
-          display3: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w700, color: config.Colors().mainDarkColor(1)),
-          display4: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w300, color: config.Colors().secondDarkColor(1)),
-          subhead: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500, color: config.Colors().secondDarkColor(1)),
-          title: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600, color: config.Colors().mainDarkColor(1)),
-          body1: TextStyle(fontSize: 12.0, color: config.Colors().secondDarkColor(1)),
-          body2: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600, color: config.Colors().secondDarkColor(1)),
-          caption: TextStyle(fontSize: 12.0, color: config.Colors().secondDarkColor(0.7)),
-        ),
-      ),
-      theme: ThemeData(
-        fontFamily: 'Poppins',
-        primaryColor: Colors.white,
-        brightness: Brightness.light,
-        accentColor: config.Colors().mainColor(1),
-        focusColor: config.Colors().textSecondeColor(1),
-        hintColor: config.Colors().textAccentColor(1),
-        textTheme: TextTheme(
-          button: TextStyle(color: Colors.white),
-          headline: TextStyle(fontSize: 20.0, color: config.Colors().textSecondeColor(1)),
-          display1: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600, color: config.Colors().textSecondeColor(1)),
-          display2: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600, color: config.Colors().textSecondeColor(1)),
-          display3: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w700, color:Colors.white),
-          display4: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w300, color: config.Colors().textSecondeColor(1)),
-          subhead: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500, color: config.Colors().textSecondeColor(1)),
-          title: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600, color: config.Colors().textMainColor(1)),
-          body1: TextStyle(fontSize: 12.0, color: config.Colors().textSecondeColor(1)),
-          body2: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600, color: config.Colors().textSecondeColor(1)),
-          caption: TextStyle(fontSize: 12.0, color: config.Colors().textAccentColor(0.6)),
+    return Scaffold(
+      backgroundColor: Theme.of(context).accentColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+                  margin: EdgeInsets.symmetric(vertical: 65, horizontal: 20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Theme.of(context).primaryColor,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Theme.of(context).hintColor.withOpacity(0.2), offset: Offset(0, 10), blurRadius: 20)
+                      ]),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 25),
+                      Text('Login', style: Theme.of(context).textTheme.display2),
+                      SizedBox(height: 20),
+                      new TextField(
+                        style: TextStyle(color: Theme.of(context).focusColor),
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: new InputDecoration(
+                          hintText: 'Endereço de email',
+                          hintStyle: Theme.of(context).textTheme.body1.merge(
+                            TextStyle(color: Theme.of(context).focusColor.withOpacity(0.6)),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Theme.of(context).focusColor.withOpacity(0.2))),
+                          focusedBorder:
+                          UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).focusColor)),
+                          prefixIcon: Icon(
+                            UiIcons.envelope,
+                            color: Theme.of(context).focusColor.withOpacity(0.6),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      new TextField(
+                        style: TextStyle(color: Theme.of(context).focusColor),
+                        keyboardType: TextInputType.text,
+                        obscureText: !_showPassword,
+                        decoration: new InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: Theme.of(context).textTheme.body1.merge(
+                            TextStyle(color: Theme.of(context).focusColor.withOpacity(0.6)),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Theme.of(context).focusColor.withOpacity(0.2))),
+                          focusedBorder:
+                          UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).focusColor)),
+                          prefixIcon: Icon(
+                            UiIcons.padlock_1,
+                            color: Theme.of(context).focusColor.withOpacity(0.6),
+                          ),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _showPassword = !_showPassword;
+                              });
+                            },
+                            color: Theme.of(context).focusColor.withOpacity(0.4),
+                            icon: Icon(_showPassword ? Icons.visibility_off : Icons.visibility),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Esqueceu sua senha ?',
+                          style: Theme.of(context).textTheme.body1,
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      FlatButton(
+                        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 70),
+                        onPressed: () {
+                          // 2 number refer the index of Home page
+                          Navigator.of(context).pushNamed('/Tabs', arguments: 2);
+                        },
+                        child: Text(
+                          'Login',
+                          style: Theme.of(context).textTheme.title.merge(
+                            TextStyle(color: Theme.of(context).primaryColor),
+                          ),
+                        ),
+                        color: Theme.of(context).accentColor,
+                        shape: StadiumBorder(),
+                      ),
+                      SizedBox(height: 50),
+                      Text(
+                        'Ou usando mídias sociais',
+                        style: Theme.of(context).textTheme.body1,
+                      ),
+                      SizedBox(height: 20),
+                      new SocialMediaWidget()
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/SignUp');
+              },
+              child: RichText(
+                text: TextSpan(
+                  style: Theme.of(context).textTheme.title.merge(
+                    TextStyle(color: Theme.of(context).primaryColor),
+                  ),
+                  children: [
+                    TextSpan(text: 'Você não tem uma conta?'),
+                    TextSpan(text: ' Inscrever-se', style: TextStyle(fontWeight: FontWeight.w700)),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
