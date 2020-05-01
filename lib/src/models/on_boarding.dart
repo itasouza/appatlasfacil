71c4160d191b53172d96fb1a05dca8b8a15291a7
itasouza <itasouza@yahoo.com.br>
2020-05-01 11:47:21 -0300

    Adicionado mais uma imagem no tela de splash

--- lib/src/models/on_boarding.dart
+++ lib/src/models/on_boarding.dart
@@ -14,8 +14,8 @@ class OnBoardingList {
     _list = [
       new OnBoarding(image: 'img/onboarding1.png', description: 'Descubra sua localização em detalhes, obtenha informações e aproveite.'),
       new OnBoarding(image: 'img/onboarding2.png', description: 'Encontre facilmente sua categoria com suporte flexível para exibição.'),
-      //new OnBoarding(image: 'images/onboarding1.png', description: 'So many books, so little time.'),
-      //new OnBoarding(image: 'images/onboarding2.png', description: 'A room without books is like a body without a soul.'),
+      new OnBoarding(image: 'img/onboarding1.png', description: 'Tantos livros, tão pouco tempo.'),
+      //new OnBoarding(image: 'img/onboarding2.png', description: 'Uma sala sem livros é como um corpo sem alma.'),
     ];
   }
 }
